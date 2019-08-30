b9fe26502a84cd4d8361a0e13458fb76775d4980
Paul Sajna <sajattack@gmail.com>
2019-08-30 07:20:38 -0700

    cluster the eclic clusterfuck

---
+++ src/eclic/clicints.rs
@@ -0,0 +1,24 @@
+#[doc = "clicintip Register"]
+pub struct CLICINTIP {
+    register: ::vcell::VolatileCell<u8>,
+}
+#[doc = "clicintip Register"]
+pub mod clicintip;
+#[doc = "clicintie Register"]
+pub struct CLICINTIE {
+    register: ::vcell::VolatileCell<u8>,
+}
+#[doc = "clicintie Register"]
+pub mod clicintie;
+#[doc = "clicintattr Register"]
+pub struct CLICINTATTR {
+    register: ::vcell::VolatileCell<u8>,
+}
+#[doc = "clicintattr Register"]
+pub mod clicintattr;
+#[doc = "clicintctl Register"]
+pub struct CLICINTCTL {
+    register: ::vcell::VolatileCell<u8>,
+}
+#[doc = "clicintctl Register"]
+pub mod clicintctl;
--- update.sh
+++ update.sh
@@ -5,7 +5,7 @@ set -e
 rm -rf src
 mkdir src
 svd patch patches/gd32vf103.yaml
-svd2rust --target riscv -i GD32VF103.svd.patched
+svd2rust --target riscv -i GD32VF103.svd.clustered
 form -i lib.rs -o src
 rm lib.rs
 cargo fmt
