afe98a9855946565e3741823d1b44b46db8465f8
Vadim Kaushan <admin@disasm.info>
2019-08-30 14:55:17 +0300

    Move fix into a separate patch file

--- update.sh
+++ update.sh
@@ -4,7 +4,8 @@ set -e
 
 rm -rf src
 mkdir src
-svd2rust --target riscv -i GD32VF103.svd
+svd patch patches/gd32vf103.yaml
+svd2rust --target riscv -i GD32VF103.svd.patched
 form -i lib.rs -o src
 rm lib.rs
 cargo fmt
