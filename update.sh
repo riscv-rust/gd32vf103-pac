be24e23cfc3ca017772dcb02ec7518e78a400bcb
Thales Fragoso <thales.fragosoz@gmail.com>
2020-11-01 17:24:58 -0300

    Update svd2rust and remove riscv-rt dependency

--- update.sh
+++ update.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 # Before running this script, install the required software:
-# cargo install svd2rust --version=0.16.1
+# cargo install svd2rust --version=0.17.0
 # cargo install form --version=0.7.0
 # pip3 install --upgrade --user svdtools
 
@@ -15,3 +15,5 @@ svd2rust --target riscv -i GD32VF103.svd.patched
 form -i lib.rs -o src
 rm lib.rs
 cargo fmt
+grep -E 'feature = "rt"|extern crate riscv_rt' src/lib.rs | tee librs-patch
+grep -Ev 'feature = "rt"|extern crate riscv_rt' src/lib.rs > librs-temp && mv librs-temp src/lib.rs
